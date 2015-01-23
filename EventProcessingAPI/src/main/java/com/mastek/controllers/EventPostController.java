61584ea4905cfdf93a38b7091c64da38b306b48f
vinayamastek <vinaya.salgaonkar@mastek.com>
2015-01-23 12:57:16 +0530

    Removed the Customerr ui apis

--- EventProcessingAPI/src/main/java/com/mastek/controllers/EventPostController.java
+++ EventProcessingAPI/src/main/java/com/mastek/controllers/EventPostController.java
@@ -15,20 +15,14 @@ import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
-import javax.ws.rs.Produces;
-import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
-
 import net.sf.json.JSONObject;
-
 import com.mastek.domain.PostgresImpl;
-import com.sun.research.ws.wadl.Application;
 
 @ServerEndpoint("/websocket/notifyAnnotation")
 @Path("/events")
 public class EventPostController {
-
 	public static class SessionRegistry {
 		static HashMap<String, Session> registry = new HashMap<String, Session>();
 
@@ -41,13 +35,10 @@ public class EventPostController {
 			return registry.get(transactionID);
 			
 		}
-
 	}
 
 	@Context
 	private HttpServletRequest request;
-
-	private String input;
 
 	// http://localhost:8080/RealAnalyticsUI/rest/events/eventget?eventdata="test";
 
@@ -114,25 +105,6 @@ public class EventPostController {
 	}
 
 
-	@GET
-	@Path("/getapplication")
-	@Produces(MediaType.APPLICATION_JSON)
-	public JSONObject GetApplication(@Context HttpServletResponse servletResponse, @QueryParam("transactionid") Integer transactionid) {
-		System.out.println(transactionid);
-		JSONObject application = fetchApplication(transactionid);
-		return application;
-	}
-
-
-	private JSONObject fetchApplication(Integer transactionId) {
-		PostgresImpl pg = new PostgresImpl();
-		pg.connect();
-		JSONObject application = pg.fetchApplication(transactionId);
-		pg.close();
-		return application;
-	}
-
-	
 	@POST
 	@Path("/closetran")
 	@Consumes(MediaType.APPLICATION_JSON)
@@ -144,62 +116,11 @@ public class EventPostController {
 
 	}
 
-	// http://localhost:8080/RealAnalyticsUI/rest/events/eventpost?eventdata="test";
-
 	private void closeTran(JSONObject edata) {
 		PostgresImpl pg = new PostgresImpl();
 		pg.connect();
 		pg.closeTran(edata);
 		pg.close();
-	}
-
-	@POST
-	@Path("/applicationupdt")
-	@Consumes(MediaType.APPLICATION_JSON)
-	public void ApplicationUpdt(Object aData,
-			@Context HttpServletResponse servletResponse) {
-		System.out.println(JSONObject.fromObject(aData));
-		updateApplication(JSONObject.fromObject(aData));
-	}
-
-	private void updateApplication(JSONObject applicationData) {
-		PostgresImpl pg = new PostgresImpl();
-		pg.connect();
-		pg.updateApplication(applicationData);
-		pg.close();
-	}
-	
-	@POST
-	@Path("/applicationsave")
-	@Consumes(MediaType.APPLICATION_JSON)
-	public void ApplicationSave(Object aData,
-			@Context HttpServletResponse servletResponse) {
-		System.out.println(JSONObject.fromObject(aData));
-		addApplication(JSONObject.fromObject(aData));
-	}
-
-
-	private void addApplication(JSONObject applicationData) {
-		PostgresImpl pg = new PostgresImpl();
-		pg.connect();
-		pg.insertApplication(applicationData);
-		pg.close();
-	}
-
-	@POST
-	@Path("/applicationfinalize")
-	@Consumes(MediaType.APPLICATION_JSON)
-	public void ApplicationFinalize(Object aData,
-			@Context HttpServletResponse servletResponse) {
-		finalizeApplication(JSONObject.fromObject(aData));
-	}
-
-	private void finalizeApplication(JSONObject applicationData) {
-		PostgresImpl pg = new PostgresImpl();
-		pg.connect();
-		pg.finalizeApplication(applicationData);
-		pg.close();
-		
 	}
 
 	@POST
@@ -214,7 +135,6 @@ public class EventPostController {
 		Session s1 = SessionRegistry.getTransaction("ad"+(String) eventData
 				.get("tran"));
 
-		//echoTextMessage(s, eventData.getString("msg"), true);
 		echoTextMessage(s, eventData.toString(), true);
 		echoTextMessage(s1, eventData.toString(), true);
 
@@ -223,7 +143,6 @@ public class EventPostController {
 	}
 
 	// http://localhost:8080/RealAnalyticsUI/rest/events/countUpdt?eventdata="{tran="111","msg="abcd "};
-
 	@OnMessage
 	public void echoTextMessage(Session session, String msg, boolean last) {
 		try {
--- EventProcessingAPI/src/main/java/com/mastek/domain/PostgresImpl.java
+++ EventProcessingAPI/src/main/java/com/mastek/domain/PostgresImpl.java
@@ -6,12 +6,8 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 
-import net.sf.json.JSON;
 import net.sf.json.JSONArray;
-import net.sf.json.JSONNull;
 import net.sf.json.JSONObject;
-import net.sf.json.util.JSONUtils;
-import net.sf.json.xml.JSONTypes;
 
 public class PostgresImpl {
 	private Connection conn = null;
@@ -47,26 +43,6 @@ public class PostgresImpl {
 
 	}
 
-	/*
-	 * public void insertEvents(String eventData) { String sql =
-	 * "INSERT INTO events"; String fld = "%s"; String fieldStr =
-	 * "("+fld+","+fld+","+fld+","+fld+","+fld+")";
-	 * 
-	 * String quote = "'"; String valueStrFld = quote + "%s" +quote; String
-	 * valueStr =
-	 * "("+valueStrFld+","+valueStrFld+","+valueStrFld+","+valueStrFld
-	 * +","+valueStrFld+")"; String[] fields = eventData.split("&");
-	 * 
-	 * fieldStr =
-	 * String.format(fieldStr,fields[0].split("=")[0],fields[1].split(
-	 * "=")[0],fields
-	 * [2].split("=")[0],fields[3].split("=")[0],fields[4].split("=")[0]);
-	 * valueStr =
-	 * String.format(valueStr,fields[0].split("=")[1],fields[1].split(
-	 * "=")[1],fields
-	 * [2].split("=")[1],fields[3].split("=")[1],fields[4].split("=")[1]); sql =
-	 * sql + fieldStr + " VALUES " + valueStr; executeSql(sql); }
-	 */
 	public void insertEvents(JSONObject eventData) {
 		String sql = "INSERT INTO events";
 		// event, payload, timestamp, sessionid
@@ -74,12 +50,6 @@ public class PostgresImpl {
 		String fieldStr = "(" + "event" + "," + "payload" + "," + "timestamp"
 				+ "," + "sessionid" + ")";
 		String quote = "'";
-		/*
-		 * String valueStr = "(" + quote + eventData.getString("event") + quote
-		 * + "," + quote + eventData.getJSONObject("payload") + quote + "," +
-		 * quote + eventData.getString("timestamp") + quote + "," + quote +
-		 * eventData.getString("sessionid") + quote + ")";
-		 */
 		String valueStr = "(" + quote + eventData.getString("event") + quote
 				+ "," + quote + eventData.getJSONObject("payload") + quote
 				+ "," + quote + eventData.getString("timestamp") + quote + ","
@@ -94,8 +64,6 @@ public class PostgresImpl {
 	public int openTran() {
 		int tranid = 0;
 		String sql = "INSERT INTO tran";
-		// event, payload, timestamp, sessionid
-
 		String fieldStr = "(" + "status" + ")";
 		String quote = "'";
 		String valueStr = "(" + quote + 'O' + quote + ")";
@@ -111,7 +79,6 @@ public class PostgresImpl {
 			while (rs.next()) {
 				tranid = rs.getInt("mtranid");
 			}
-
 			return tranid;
 		} catch (SQLException e) {
 			e.printStackTrace();
@@ -155,8 +122,6 @@ public class PostgresImpl {
 
 		String sql = "update tran set status = " + quote + "C" + quote
 				+ " where tranid=" + edata.getString("tranid");
-		// event, payload, timestamp, sessionid
-
 		System.out.println(sql);
 		executeSql(sql);
 	}
@@ -178,7 +143,6 @@ public class PostgresImpl {
 				stmt.close();
 				commit();
 			} catch (SQLException e) {
-				// TODO Auto-generated catch block
 				e.printStackTrace();
 			}
 		}
@@ -222,170 +186,6 @@ public class PostgresImpl {
 			e.printStackTrace();
 		}
 		return results;
-	}
-
-	public void insertApplication(JSONObject applicationData) {
-		String sql = "INSERT INTO application";
-		String fieldStr = "(" + "title" +","+ "firstname" + "," + "lastname" + ","
-				+ "dob" + "," + "maritalstatus" + "," + "address" + ","
-				+ "pincode" + "," + "email" + "," + "emptype" + ","
-				+ "industry" + "," + "annualincome" + "," + "loanamount" + "," +"transactionid" + ","+"timestamp" + ","
-				 + "passportnumber"  + "," + "passportdate"  + ")";
-
-		String quote = "'";
-		String pdt = null;
-		String dt = null;
-
-		if (JSONUtils.isNull(applicationData.get("dob")))
-			dt = null;
-		else dt =quote+applicationData.get("dob")+quote;
-
-		if (JSONUtils.isNull(applicationData.get("passportdate")))
-			pdt = null;
-		else pdt =quote+applicationData.get("passportdate")+quote;
-
-		String valueStr = "(" + checkNull(applicationData.getString("title"))
-				+ "," + checkNull(applicationData.getString("firstName"))
-				+ "," + checkNull(applicationData.getString("lastName"))
-				+ "," + dt
-				+ "," + checkNull(applicationData.getString("maritalStatus"))
-				+ "," + checkNull(applicationData.getString("address"))
-				+ "," + checkNull(applicationData.getString("pincode"))
-				+ "," + checkNull(applicationData.getString("email"))
-				+ "," + checkNull(applicationData.getString("emptype"))
-				+ "," + checkNull(applicationData.getString("industry"))
-				+ "," + applicationData.get("annualincome") 
-				+ "," + applicationData.get("loanamount") 
-				+ "," + applicationData.getInt("transactionid") 
-				+ "," + applicationData.getString("timestamp") 
-				+ "," + checkNull(applicationData.getString("passportnumber")) 
-				+ "," + pdt
-				+ ")";
-		
-			
-		sql = sql + fieldStr + " VALUES " + valueStr;
-		System.out.println(sql);
-		executeSql(sql);
-	}
-
-	
-	
-	private String checkNull(String value)
-	{
-		String quote = "'";
-		if (value == null)
-			return null;
-		else 
-			return quote + value + quote;
-	}
-	
-	
-	public JSONObject fetchApplication(Integer transactionId) {
-		JSONObject jobj = new JSONObject();
-		if (conn == null)
-			connect();
-		String sql = "";
-		ResultSet rs = null;
-		sql = "SELECT " + "	id,title,firstname,lastname,dob,"
-				+ "maritalstatus, emptype,pincode, address, "
-				+ "industry, annualincome, loanamount, email,status, "
-				+ "passportnumber,passportdate, transactionid, "
-				+ "timestamp " + "FROM application where transactionid = "
-				+ transactionId;
-		System.out.println(sql);
-		try {
-			stmt = conn.createStatement();
-			rs = stmt.executeQuery(sql);
-			while (rs.next()) {
-				jobj.put("id", rs.getInt("id"));
-				jobj.put("title", rs.getString("title"));
-				jobj.put("firstname", rs.getString("firstname"));
-				jobj.put("lastname", rs.getString("lastname"));
-				jobj.put("dob", rs.getString("dob"));
-				jobj.put("maritalstatus", rs.getString("maritalstatus"));
-				jobj.put("emptype", rs.getString("emptype"));
-				jobj.put("pincode", rs.getString("pincode"));
-				jobj.put("address", rs.getString("address"));
-				jobj.put("industry", rs.getString("industry"));
-				jobj.put("annualincome", rs.getDouble("annualincome"));
-				jobj.put("loanamount", rs.getDouble("loanamount"));
-				jobj.put("email", rs.getString("email"));
-				jobj.put("status", rs.getString("status"));
-				jobj.put("passportnumber", ""); 
-				jobj.put("passportdate", "");
-				jobj.put("transactionid", rs.getInt("transactionid"));
-				jobj.put("timestamp", rs.getString("timestamp"));
-				break;
-			}
-			rs.close();
-			stmt.close();
-		} catch (SQLException e) {
-			e.printStackTrace();
-		}
-		return jobj;
-	}
-
-	public void finalizeApplication(JSONObject applicationData) {
-		String sql = "UPDATE application SET status = %s WHERE applicationid = %s";
-		String quote = "'";
-		sql = String.format(sql, quote + applicationData.getString("status")
-				+ quote, applicationData.getString("id"));
-		System.out.println(sql);
-		executeSql(sql);
-	}
-
-	public void updateApplication(JSONObject applicationData) {
-		String sql = "UPDATE application SET "
-				+ "title = %s, "
-				+ "firstname = %s, "
-				+ "lastname = %s, "
-				+ "dob = %s, "
-				+ "maritalstatus = %s, "
-				+ "pincode= %s, "
-				+ "address = %s, "
-				+ "emptype = %s, "
-				+ "industry = %s, "
-				+ "annualincome= %s, "
-				+ "loanamount= %s, "
-				+ "email= %s, "
-				+ "passportnumber = %s, "
-				+ "passportdate = %s, "
-				+ "status = %s,  "
-				+ "timestamp = %s"
-				+ " WHERE "
-				+ " id = %s";
-		String quote = "'";
-		String dt=null;
-		String pdt =null;
-		
-		if (JSONUtils.isNull(applicationData.get("dob")))
-			dt = null;
-		else dt =quote+applicationData.get("dob")+quote;
-
-		if (JSONUtils.isNull(applicationData.get("passportdate")))
-			pdt = null;
-		else pdt =quote+applicationData.get("passportdate")+quote;				
-				
-		sql = String.format(sql, 
-				checkNull( applicationData.getString("title") ) ,
-				checkNull( applicationData.getString("firstName") ) ,
-				checkNull( applicationData.getString("lastName") ) ,
-				dt ,
-				checkNull( applicationData.getString("maritalStatus") ) ,
-				checkNull( applicationData.getString("pincode") ) ,
-				checkNull( applicationData.getString("address") ) ,
-				checkNull( applicationData.getString("emptype") ) ,
-				checkNull( applicationData.getString("industry") ) ,
-				applicationData.getDouble("annualincome"),
-				applicationData.getDouble("loanamount"),
-				checkNull( applicationData.getString("email") ) ,
-				checkNull( applicationData.getString("passportnumber") ) ,
-				pdt ,
-				checkNull( applicationData.getString("status") ) ,
-				checkNull( applicationData.getString("timestamp") ),
-				applicationData.getString("id"));
-		System.out.println(sql);
-		executeSql(sql);
 	}
 
 }
